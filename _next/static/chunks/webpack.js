e0a0e5a91386563fa7a5015f70b22ce3c3df89e2
yanhenning <yanhenning@gmail.com>
2021-06-15 18:15:18 -0300

    Deploy Next.js to gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4e929db26358abe6923e";
+/******/ 	var hotCurrentHash = "5fef06ffa0c672b0afcb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
